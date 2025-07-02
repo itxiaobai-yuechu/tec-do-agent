beb9a649e8970f59b7aecd565d9955605d1bfe4d
dzj <945435098@qq.com>
2025-07-02 16:39:58 +0800

    remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from src.utils import html_to_text
-
-if __name__ == "__main__":
-    html_content = """
-<div class="raw-message hideM"><pre class="fake-pre">你好！有什么我可以帮助你的吗？</pre></div><div class="md-message">\n\n你好！有什么我可以帮助你的吗？\n</div>
-    """
-    print(html_to_text(html_content))
