9d560a9f7e5545d478d5b721b1e3b09b05392ff7
dzj <945435098@qq.com>
2025-07-03 16:35:45 +0800

    create css selector

---
+++ agent/selector.py
@@ -0,0 +1,18 @@
+
+
+selectors = {
+    "lazada": {
+        "title": "h1.pdp-mod-product-badge-title",
+        "price": "span.pdp-v2-product-price-content-salePrice-amount",
+        "original_price": "span.pdp-v2-product-price-content-originalPrice-amount",
+        "discount": "span.pdp-v2-product-price-content-originalPrice-discount",
+        "images": "img.gallery-preview-panel-v2__image",
+        "score": "span.container-star-v2-score",
+        "rating_count": "a.pdp-review-summary-v2__link span",
+        "reviews": "div.item-content-main-content-reviews-item span",
+        "detail_text": "div.html-content.detail-content article p span",
+        "detail_images": "div.html-content.detail-content article p img",
+        "brand": "a.pdp-product-brand-v2__brand-link",
+        "product_description": "div.pdp-product-desc"
+    }
+}
